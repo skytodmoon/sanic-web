450583dba19945c9073d3fae5718899c9a4218a4
lihuan <343397495@qq.com>
2024-11-18 14:50:50 +0800

    readme update

--- constants/code_enum.py
+++ constants/code_enum.py
@@ -20,9 +20,9 @@ class DiFyAppEnum(Enum):
     DiFy app-key 枚举
     """
 
-    DATABASE_QA = ("DATABASE_QA", "app-GOyZYgPPO3bg2OvfZIfjGfFz", "数据问答")
+    DATABASE_QA = ("DATABASE_QA", "app-HFjPprweT1OJDckUh71JQd3T", "数据问答")
 
-    FILEDATA_QA = ("FILEDATA_QA", "app-lL9IirirLhuzbmqipKH6qGZO", "文件问答")
+    FILEDATA_QA = ("FILEDATA_QA", "app-lL9IirirLhuzbmqipKH6qGZO", "表格问答")
 
 
 class DataTypeEnum(Enum):
